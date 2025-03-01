ee4d039b89d6352dd1a7f2b50d05ac96c0a6cb99
YumNumm <onory0917@gmail.com>
2025-03-02 01:49:00 +0900

    remove eq* package

--- packages/eq_flutter/android/app/src/main/kotlin/com/example/eq_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.eq_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- packages/eq_server/bin/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:eq_server/server.dart';
-
-void main(List<String> args) {
-  run(args);
-}
