771a1a1ff982f252174445c4822092bc36fd2cd1
YumNumm <onory0917@gmail.com>
2025-03-01 23:25:18 +0900

    add api

--- api/lib/main.dart
+++ api/lib/main.dart
@@ -7,13 +7,24 @@ import 'package:http/http.dart';
 @JS('fetch')
 external JSPromise<JSResponse> fetch(JSString input);
 
-void main() =>
-    Workers((request) async {
+Future<void> main() async =>
+    Workers((request, env, ctx) async {
       final response =
           await fetch(
             'https://jsonplaceholder.typicode.com/todos/1'.toJS,
           ).toDart;
       print((await response.toDart).body);
 
+      final environment = JSEnv(env);
+      print(environment.supabaseKey);
+
       return Response('OK', 200).toJS;
     }).serve();
+
+extension type JSEnv(JSObject _) implements JSObject {
+  @JS('SUPABASE_URL')
+  external String get supabaseUrl;
+
+  @JS('SUPABASE_KEY')
+  external String get supabaseKey;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,35 @@
 import { instantiate, invoke } from '../build/main.mjs';
 import mod from '../build/main.wasm';
+import type {
+  ExecutionContext,
+  ExportedHandler,
+  Response as CFResponse,
+  Request as CFRequest,
+} from '@cloudflare/workers-types';
 
 const dartInstance = await instantiate(mod);
 
-export default {
-  async fetch(request: Request) {
-    return executeDartWorker(request);
-  },
-};
-
-async function executeDartWorker(request: Request) {
-  return new Promise((resolve) => {
-    globalThis.__dart_cf_workers = {
-      request: () => request,
-      response: (response) => resolve(response),
-    };
-    invoke(dartInstance, request);
-  });
+// グローバル型定義を追加
+declare global {
+  var __dart_cf_workers: {
+    request: () => CFRequest;
+    response: (response: Response) => void;
+    env: () => Env;
+    ctx: () => ExecutionContext;
+  };
 }
 
-fetch;
+export default {
+  async fetch(request: CFRequest, env, ctx): Promise<CFResponse> {
+    return new Promise<CFResponse>((resolve) => {
+      globalThis.__dart_cf_workers = {
+        request: () => request,
+        // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+        response: (response: any) => resolve(response as unknown as CFResponse),
+        env: () => env,
+        ctx: () => ctx,
+      };
+      invoke(dartInstance, request);
+    });
+  },
+} satisfies ExportedHandler<Env>;
